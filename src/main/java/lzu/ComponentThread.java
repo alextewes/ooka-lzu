839fd729fba59b3249c3f5824916b8192d69d8ee
Alexander Tewes <alexandertewes7@gmail.com>
2023-04-18 22:17:58 +0200

    Implement ComponentThread

---
+++ src/main/java/lzu/ComponentThread.java
@@ -0,0 +1,30 @@
+package lzu;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ComponentThread extends Thread {
+    private ComponentInstance componentInstance;
+    private Method startMethod;
+    private Method stopMethod;
+    private Object startMethodInstance;
+    private Object stopMethodInstance;
+
+    public ComponentThread(ComponentInstance componentInstance, Method startMethod, Method stopMethod, Object startMethodInstance, Object stopMethodInstance) {
+        this.componentInstance = componentInstance;
+        this.startMethod = startMethod;
+        this.stopMethod = stopMethod;
+        this.startMethodInstance = startMethodInstance;
+        this.stopMethodInstance = stopMethodInstance;
+    }
+
+    @Override
+    public void run() {
+        try {
+            componentInstance.setState(ComponentState.RUNNING);
+            startMethod.invoke(startMethodInstance);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
