4d7b2bc7c3fa5e852217d8d4edc0ad5fb08ecda8
alextewes <55658240+alextewes@users.noreply.github.com>
2023-05-07 18:10:54 +0200

    Add express API for managing LZU states

---
+++ component-state-api/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+
+const dataFilePath = './component_state.json';
+
+app.post('/api/save-config', (req, res) => {
+    fs.writeFile(dataFilePath, JSON.stringify(req.body, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ message: 'An error occurred while saving the configuration.' });
+        } else {
+            res.status(200).send({ message: 'Configuration saved successfully.' });
+        }
+    });
+});
+
+app.get('/api/load-config', (req, res) => {
+    fs.readFile(dataFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ message: 'An error occurred while loading the configuration.' });
+        } else {
+            res.status(200).send(JSON.parse(data));
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
