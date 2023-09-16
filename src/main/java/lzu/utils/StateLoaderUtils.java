563a21cd9f553ed599fbb0a76cdb66264551bbc9
Alexander Tewes <alexandertewes7@gmail.com>
2023-09-16 14:25:39 +0200

    Refactor StateLoading

--- src/main/java/lzu/utils/StateLoaderUtils.java
+++ src/main/java/lzu/utils/StateLoaderUtils.java
@@ -1,46 +1,33 @@
 package lzu.utils;
 
+import lzu.model.ComponentInstance;
 import lzu.service.ComponentLoader;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StateLoaderUtils {
 
-    public static List<Map<String, String>> loadComponentStates(String filePath) throws IOException {
-        Path file = Paths.get(filePath);
-
-        String jsonState = Files.readString(file);
-        JSONObject jsonObject = new JSONObject(jsonState);
-        JSONArray componentInstancesArray = jsonObject.getJSONArray("componentInstances");
+    public static List<Map<String, String>> loadComponentStates(String filename) throws IOException {
+        String jsonStr = new String(Files.readAllBytes(Paths.get(filename)));
+        JSONObject jsonObj = new JSONObject(jsonStr);
+        JSONArray componentInstances = jsonObj.getJSONArray("componentInstances");
 
         List<Map<String, String>> componentStates = new ArrayList<>();
-        for (int i = 0; i < componentInstancesArray.length(); i++) {
-            JSONObject componentInstanceObject = componentInstancesArray.getJSONObject(i);
-            String name = componentInstanceObject.getString("name");
-            Path jarFilePath = Path.of(componentInstanceObject.getString("jarFilePath"));
-            String state = componentInstanceObject.getString("state");
-            String componentId = componentInstanceObject.getString("ID");
+        for (int i = 0; i < componentInstances.length(); i++) {
+            JSONObject instance = componentInstances.getJSONObject(i);
+            String name = instance.getString("name");
+            String jarFilePath = instance.getString("jarFilePath");
 
-            Map<String, String> componentState = new HashMap<>();
-            componentState.put("name", name);
-            componentState.put("jarFilePath", jarFilePath.toString());
-            componentState.put("state", state);
-            componentState.put("ID", componentId);
-
-            componentStates.add(componentState);
+            componentStates.add(Map.of("name", name, "jarFilePath", jarFilePath));
         }
-
         return componentStates;
     }
 
@@ -50,56 +37,6 @@ public class StateLoaderUtils {
             Path jarFilePath = Path.of(componentState.get("jarFilePath"));
 
             loader.deployComponent(jarFilePath, name);
-        }
-    }
-
-
-    public static ResponseEntity<String> saveState(LoadBalancer loadBalancer, String filePath) {
-        try {
-            JSONArray allComponentInstancesArray = new JSONArray();
-
-            for (ComponentLoader loader : loadBalancer.getAllLoaders()) {
-                JSONArray loaderComponentInstancesArray = loader.saveState();
-                for (int i = 0; i < loaderComponentInstancesArray.length(); i++) {
-                    allComponentInstancesArray.put(loaderComponentInstancesArray.getJSONObject(i));
-                }
-            }
-
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("componentInstances", allComponentInstancesArray);
-
-            String jsonState = jsonObject.toString();
-
-            Path file = Paths.get(filePath);
-            Files.write(file, jsonState.getBytes());
-
-            return new ResponseEntity<>("States saved successfully", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error saving state: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    public static ResponseEntity<String> loadState(LoadBalancer loadBalancer, String filePath) {
-        try {
-            List<Map<String, String>> componentStates = loadComponentStates(filePath);
-            List<ComponentLoader> loaders = loadBalancer.getAllLoaders();
-            int numLoaders = loaders.size();
-
-            int totalComponents = componentStates.size();
-            int componentsPerLoader = totalComponents / numLoaders;
-            int extraComponents = totalComponents % numLoaders;
-
-            int startIndex = 0;
-            for (int i = 0; i < numLoaders; i++) {
-                int endIndex = startIndex + componentsPerLoader + (i < extraComponents ? 1 : 0);
-                List<Map<String, String>> loaderComponentStates = componentStates.subList(startIndex, endIndex);
-                loadStateForLoader(loaders.get(i), loaderComponentStates);
-                startIndex = endIndex;
-            }
-
-            return new ResponseEntity<>("State loaded successfully", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error loading state: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
